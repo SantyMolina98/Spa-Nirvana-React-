77a8a5438e081193fd312e831b1c1d11146036a6
Santiago Molina <santymolina0798@gmail.com>
2025-09-19 21:08:03 -0300

    se cambiaron nombre de imagenes y modificaciones en header

--- src/components/headerComponents.jsx
+++ src/components/headerComponents.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react';
 import '../styles/App.css';
+import { NavLink } from 'react-router-dom';
 import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 function HeaderComponent () {
   return (
-    <Fragment>
-      {/* <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      /* <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img src="assets/img/Logo-Spa.png" alt="logo" className="logo" />
@@ -66,31 +66,36 @@ function HeaderComponent () {
           </div>
         </div>
       </nav>
-      {/* El resto de tu código que sigue al navbar */}
+      {/* El resto de tu código que sigue al navbar */
       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+        <Container>
+          <Navbar.Brand href="/">Imagen de Inicio</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink to="/">Inicio</NavLink>
+              <NavLink to="/turnos">Turnos</NavLink>
+              <NavDropdown title="Categorías" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/categorias">Tratamientos Faciales</NavDropdown.Item>
+                <NavDropdown.Item href="/categorias">
+                  Tratamientos Corporales
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/categorias">Masajes</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/categorias">
+                  Masajes con Aromaterapia
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/categorias">
+                  Rituales
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavLink to="/contacto">Contacto</NavLink>
+              <NavLink to="/nosotros">Nosotros</NavLink>
+              <NavLink to="/login">Imagen de login</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
     </Navbar>
-    </Fragment>
-  );
-};
+  )
+}
 export default HeaderComponent;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import HeaderComponent from '../components/headerComponents';
-
+import { HomePage } from './HomePage';
+import { Turnos } from './Turnos';
+import { Categorias } from './Categorias';
+import { Contacto } from './Contacto';
+import { Nosotros } from './Nosotros';
+import { Login } from './Login';
 
 function App() {
   
   return (
-    <>
+    <BrowserRouter>
       <HeaderComponent/>
-    </>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/turnos' element={<Turnos/>}/>
+        <Route path='/categorias' element={<Categorias/>}/>
+        <Route path='/contacto' element={<Contacto/>}/>
+        <Route path='/nosotros' element={<Nosotros/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
