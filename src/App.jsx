2f37546b023fc2339a2f4c1c6d17486c7aa0ba70
Nazarena <nazarenamolina2@gmail.com>
2025-10-07 21:06:56 -0300

    corrección de errores de ruteo para las pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import HeaderComponent from './components/headerComponents.jsx';
-import './styles/headerComponent.css';
-import HomePage from './pages/HomePage.jsx';
-import Turnos from './pages/Turnos.jsx';
-import Categorias from './pages/Categorias.jsx';
-import ServiciosMasajes from './pages/serviciosmasaje.jsx';
-import ServiciosFacial from './pages/serviciosfacial.jsx';
-import Contacto from './pages/Contacto.jsx';
-import Nosotros from './pages/Nosotros.jsx';
-import Login from './pages/Login.jsx';
-import Registro from './pages/Registro.jsx';
-import PaginaError from './pages/PaginaError.jsx';
-import FooterComponent from './components/footerComponents.jsx';
+import HeaderComponents from './components/headerComponents';
+import HomePage from './pages/HomePage';
+import Turnos from './pages/Turnos';
+import { Categorias } from './pages/Categorias';
+import Contacto from './pages/Contacto';
+import Nosotros from './pages/Nosotros';
+import Login from './pages/Login';
+import Registro from './pages/Registro';
+import PaginaError from './pages/PaginaError';
+import FooterComponents from './components/FooterComponents';
+
 
 function App() {
   
   return (
     <>
     <BrowserRouter>
-      <HeaderComponent/>
+      <HeaderComponents/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/turnos' element={<Turnos/>}/>
         <Route path='/categorias' element={<Categorias/>}/>
-        <Route path='/categorias/serviciosmasajes' element={<ServiciosMasajes/>}/>
-        <Route path='/categorias/serviciosfacial' element={<ServiciosFacial/>}/>
         <Route path='/contacto' element={<Contacto/>}/>
         <Route path='/nosotros' element={<Nosotros/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/login/registro' element={<Registro/>}/>
         <Route path='*' element={<PaginaError/>}/>
       </Routes>
-      <FooterComponent/>
+      <FooterComponents/>
     </BrowserRouter>
     
     </>
