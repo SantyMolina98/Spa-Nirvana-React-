c3362b796de427aa0998b5ae6971181bad2f7104
Santiago Molina <santymolina0798@gmail.com>
2025-10-13 11:09:45 -0300

    se agregó footer, pero queda ubicarlo correctamente

---
+++ src/components/footer.jsx
@@ -0,0 +1,78 @@
+import '../App.css';
+
+export default function Footer() {
+  return (
+    <footer className="row row-cols-1 row-cols-md-5 py-5 my-5 border-top">
+      <div className="col mb-3">
+        <a href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none" aria-label="Bootstrap">
+        </a>
+        <p className="text-body-secondary">© 2025</p>
+      </div>
+
+      <div className="col mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Home</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Features</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Pricing</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">FAQs</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">About</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="col mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Home</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Features</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Pricing</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">FAQs</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">About</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="col mb-3">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Home</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Features</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">Pricing</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">FAQs</a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-body-secondary">About</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+
