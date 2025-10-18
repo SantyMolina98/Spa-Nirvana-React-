226daa567d55bc9c4d4a54da41fe117b118dff84
Santiago Molina <santymolina0798@gmail.com>
2025-10-18 17:41:38 -0300

    se añadió mapa a contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -10,7 +10,10 @@ function Contacto () {
         <section id="ContainerMapa">
           <div className="Mapa">
             <h2 id="Contacto"> Vias de acceso para llegar a <br/> Nirvana Spa</h2>
-              
+              <div className='MapaContainer'>
+                <iframe title='Mapa-Spa' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.106067949513!2d-65.20974192563601!3d-26.836578490025058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1760819200251!5m2!1ses-419!2sar" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"> 
+                </iframe>
+              </div>
           </div>
           <div className="Mapa">
             <h3 id="direccion">
@@ -30,11 +33,11 @@ function Contacto () {
                     <input type="text" className="form-control" id="apellido-contacto" placeholder="Ej: Rodriguez"/>
                 </div>
                 <div>
-                  <label htmlFor="email-contacto" className="form-label">Email*</label>
+                  <label htmlFor="email-contacto" className="form-label">Email</label>
                     <input type="email" className="form-control" id="email-contacto" placeholder="name@example.com"/>
                 </div>
                 <div>
-                  <label htmlFor="textarea-contacto" className="form-label">Dejanos tu consulta*</label>
+                  <label htmlFor="textarea-contacto" className="form-label">Dejanos tu consulta</label>
                     <textarea className="form-control" id="textarea-contacto" rows="3"></textarea>       
                 </div>
                 <div>
