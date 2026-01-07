54d8fe0b0d2a40a9761c505111052363aa41e31e
Santiago Molina <santymolina0798@gmail.com>
2026-01-07 12:36:51 -0300

    se configuraron apis, además de agregar estilos en panel de admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,12 +1,16 @@
 import '../App.css';
 import '../styles/admin.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+
 import { getUsuarios, crearUsuario, actualizarUsuario, eliminarUsuario } from '../helpers/UsuariosApi.js';
 import { getCategorias, getCategoriaById, crearCategoria, actualizarCategoria, eliminarCategoria } from '../helpers/CategoriaApi.js';
 import { getServicios , getServicioById, crearServicio, actualizarServicio, eliminarServicio,  } from '../helpers/ServicioApi.js';
 import { getReservas,  } from '../helpers/ReservasApi.js';
+import { UserContext } from '../context/UserContext.jsx';
+import { Button, Form, FormControl } from 'react-bootstrap';
 
 export default function Admin() {
+  const {user} = useContext(UserContext);
   const [usuarios, setUsuarios] = useState([]);
   const [reservas, setReservas] = useState([]);
   const [categorias, setCategorias] = useState([]);
@@ -26,7 +30,7 @@ export default function Admin() {
         setReservas(reservasData.reservas || []);
         setCategorias(categoriasData.categorias || []);
         setServicios(serviciosData.servicios || []);
-        setLoading(false);
+        
       } catch (err) {
         setError('Error al cargar los datos');
         console.error(err);
@@ -37,45 +41,75 @@ export default function Admin() {
     fetchData();
   }, []);
 
-  const admins = usuarios.filter(user => user.rol === 'Admin');
-  const regularUsers = usuarios.filter(user => user.rol !== 'Admin');
+  const admins = usuarios.filter(usuario => usuario.rol === 'Admin');
+  const regularUsers = usuarios.filter(usuario => usuario.rol !== 'Admin');
 
-  if (loading) return <div className="AdminPage"><h2>Cargando...</h2></div>;
-  if (error) return <div className="AdminPage"><h2>{error}</h2></div>;
+  if (loading) return <div className="TextoCargando"><h2>Cargando...</h2></div>;
+  if (error) return <div className="TextoError"><h2>{error}</h2></div>;
 
   return (
     <main className="AdminPage">
-      <h2>Panel de Administración</h2>
-      <section>
-        <h3>Usuarios</h3>
+      <h2 className='PanelAdmin'>Panel de Administración. Bienvenido/a {user?.nombre}</h2>
+      <div className='ContenidoAdmin'>
+        <section>
+        <h3 className='TituloSeccion'>Usuarios</h3>
+        <Form>
+          <Form.Control type='search' placeholder='Buscar usuarios por ID' aria-label='Search'/>
+          <Button type='submit'>Buscar</Button>
+        </Form>
+        <hr />
         <ul>
-          {regularUsers.map(user => (
-            <li key={user._id}>{user.nombre} - {user.email}</li>
+          {regularUsers.map(usuario => (
+            <li key={usuario._id}>
+              Usuario: {usuario.nombre} - Email: {usuario.email}
+            </li>
           ))}
+          <li>
+            Total de Usuarios: {regularUsers.length}
+          </li>
         </ul>
-      </section>
-
-      <section>
-        <h3>Administradores</h3>
+        </section>
+        <hr />
+        <section>
+        <h3 className='TituloSeccion'>Administradores</h3>
+        <Form>
+          <Form.Control type='search' placeholder='Buscar administradores por ID' aria-label='Search'/>
+          <Button type='submit'>Buscar</Button>
+        </Form>
+        <hr />
         <ul>
-          {admins.map(user => (
-            <li key={user._id}>{user.nombre} - {user.email}</li>
+          {admins.map(usuario => (
+            <li key={usuario._id}>
+              Administrador: {usuario.nombre} - Email: {usuario.email}
+            </li>
           ))}
+          <li>
+            Total de Administradores: {admins.length}
+          </li>
         </ul>
-      </section>
-
-      <section>
-        <h3>Reservas</h3>
+        </section>
+        <hr />
+        <section>
+        <h3 className='TituloSeccion'>Reservas</h3>
+        <Button>Añadir Reserva</Button>
+        <Button>Eliminar Reserva</Button>
+        <hr />
         <ul>
           {reservas.map(reserva => (
             <li key={reserva._id}>
               Servicio: {reserva.servicio}, Fecha: {new Date(reserva.fechaReserva).toLocaleDateString()}, Usuario: {reserva.usuario?.nombre}
             </li>
           ))}
+          <li>
+            Total de Reservas: {reservas.length}
+          </li>
         </ul>
-      </section>
-      <section>
-        <h3>Categorías</h3>
+        </section>
+        <hr />
+        <section>
+        <h3 className='TituloSeccion'>Servicios</h3>
+        <Button>Añadir Servicio</Button>
+        <hr />
         <ul>
           {categorias.map(categoria => (
             <li key={categoria._id}>
@@ -84,12 +118,16 @@ export default function Admin() {
           ))}
           {servicios.map(servicio => (
             <li key={servicio._id}>
-              Servicio: {servicio.nombre}, Descripción: {servicio.descripcion}, Duración: {servicio.duracion} mins, Precio: ${servicio.precio}
+              Servicio: {servicio.nombre}, Descripción: {servicio.descripcion}, Duración: {servicio.duracion} , Precio: ${servicio.precio}, <Button>Editar</Button>  <Button>Eliminar</Button>
             </li>
           ))}
+          <li>
+            Total de Servicios: {servicios.length}
+          </li>
         </ul>
         
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
